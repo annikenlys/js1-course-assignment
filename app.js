367d95a11c1325ea0a995bf2979cb4e9c77cdc70
Anniken Lysø <annlys01282@stud.noroff.no>
2024-02-24 23:20:55 +0100

    api fetch to async function added error handeling and loading indicator

--- app.js
+++ app.js
@@ -1,25 +1,59 @@
 const productList = document.getElementById("product-list");
-
 let productData = [];
 
-fetch("https://api.noroff.dev/api/v1/gamehub")
-  .then((Response) => Response.json())
-  .then((productResultData) => {
-    productData = productResultData;
+// Fetch data from the API
+async function fetchData() {
+  const loadingIndicator = document.createElement("div");
+  loadingIndicator.textContent = "Loading products...";
+  productList.appendChild(loadingIndicator);
 
-    for (const product of productData) {
-      displayProduct(product);
+  try {
+    const response = await fetch("https://api.noroff.dev/api/v1/gamehub");
+    const productResultData = await response.json();
+
+    if (productResultData.length > 0) {
+      productList.removeChild(loadingIndicator);
+      productData = productResultData;
+      for (const product of productData) {
+        displayProduct(product);
+      }
+    } else {
+      console.warn("No data received from API");
     }
-  });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
+fetchData();
+
+// Display the data from the API
 function displayProduct(product) {
   const productDiv = document.createElement("div");
-  const productTitlePara = document.createElement("p");
-  productTitlePara.innerText = product.title;
-  const productImg = document.createElement("img");
-  productImg.src = product.image;
-  productImg.alt = "Picture of the video game";
-  productDiv.appendChild(productImg);
-  productDiv.appendChild(productTitlePara);
+
+  productDiv.addEventListener("click", () => {
+    navigateToProductDetails(product);
+  });
+
+  if (product.onSale) {
+    productDiv.innerHTML += `
+    <img src="${product.image}" alt="video game cover">
+    <h2>${product.title}</h2>
+    <h3 class="discounted-price">${product.discountedPrice}</h3>
+    <h3 class="before-price">${product.price}</h3>
+    `;
+  } else {
+    productDiv.innerHTML += `
+    <img src="${product.image}" alt="video game cover">
+    <h2>${product.title}</h2>
+    <h3>${product.price}</h3>
+    `;
+  }
   productList.appendChild(productDiv);
+}
+
+// Function to navigate to product details page
+function navigateToProductDetails(product) {
+  sessionStorage.setItem("selectedProduct", JSON.stringify(product));
+  window.location.href = "pages/products/product.html";
 }
